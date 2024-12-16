5599869b72215c4e5064cb2235faa63d1d7b9327
전인철 <adadad1424@gmail.com>
2024-12-16 22:05:46 +0900

    Create SmartPhoneExample.java

---
+++ chapter11/sec01/exam03/SmartPhoneExample.java
@@ -0,0 +1,14 @@
+package sec01.exam03;
+
+public class SmartPhoneExample {
+
+	public static void main(String[] args) {
+		SmartPhone myPhone=new SmartPhone("구글","안드로이드");
+		
+		String strObj=myPhone.toString(); // 재정의된 toString()호출 
+		System.out.println(strObj);
+		
+		System.out.println(myPhone); // 재정의된 toString()을 호출하고 리턴값을 받아 출력
+	}
+
+}
