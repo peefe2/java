1b18299bfb2d6aba504dbee33d8e7e749665756b
전인철 <adadad1424@gmail.com>
2024-12-16 21:55:37 +0900

    Create NullPointerExceptionExmple.java

---
+++ chapter10/sec01/exam01/NullPointerExceptionExmple.java
@@ -0,0 +1,9 @@
+package sec01.exam01;
+
+public class NullPointerExceptionExmple {
+
+	public static void main(String[] args) {
+		String data=null;
+		System.out.println(data.toString()); // 객체참조 x
+	}
+}
